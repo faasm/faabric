a647d0d8e8c65f9462719c970ee803dbf6401807
Simon Shillaker <scs17@ic.ac.uk>
2020-09-07 17:23:06 +0000

    Added hello world example

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,11 +1,13 @@
 from invoke import Collection
 
 from . import build
+from . import call
 from . import container
 from . import dev
 
 ns = Collection(
     build,
+    call,
     container,
     dev,
 )
---
+++ tasks/call.py
@@ -0,0 +1,23 @@
+import requests
+from invoke import task
+
+
+@task
+def call(ctx, user, func, data=None, host="localhost"):
+    msg = {
+            "user": user,
+            "function": func,
+    }
+
+    if data:
+        msg["input"] = data
+
+    url = "http://{}:8080/f/{}/{}".format(host, user, func)
+    response = requests.post(url, json=msg)
+    
+    if response.status_code >= 400:
+        print("Request failed: status = {}".format(response.status_code))
+        exit(1)
+
+    print(response.text)
+
