1bef5661aecfd6093ad0a72306c18e20b0ff1da8
Carlos Segarra <carlossegarragonzalez@gmail.com>
2021-03-11 17:01:08 +0000

    removing the ifdef

--- include/faabric/mpi/mpi.h
+++ include/faabric/mpi/mpi.h
@@ -70,11 +70,7 @@ extern "C"
         int worldId;
         int rank;
         int size;
-#ifdef FAABRIC_NATIVE
         void* basePtr;
-#else
-    int wasmPtr;
-#endif
         int dispUnit;
     };
 
