81a2e4a97d92dff0683d335be58fe83346a3916a
Simon Shillaker <scs17@ic.ac.uk>
2021-05-14 10:40:23 +0000

    Fix running hello world dist-test

---
+++ dist-test/master.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+
+echo "This is the master"
+
--- dist-test/run.sh
+++ dist-test/run.sh
@@ -1,31 +1,47 @@
 #!/bin/bash
 
 set -e
-set -x
 
 THIS_DIR=$(dirname $(readlink -f $0))
-PROJ_ROOT=${THIS_DIR}/..
+export PROJ_ROOT=${THIS_DIR}/..
 
 if [ -z "$1" ]; then
     echo "Must provide a mode, ci or local"
     exit 1
 elif [ "$1" == "ci" ]; then
     echo "Running in CI mode"
+
+    export FAABRIC_LOCAL_BUILD_DIR=${PROJ_ROOT}/build
+    export FAABRIC_BUILD_MOUNT=/build/local-build
 elif [ "$1" == "local" ]; then
     echo "Running in local mode"
+
+    # Assume this is running as part of the Faasm dev set-up
+    export FAABRIC_LOCAL_BUILD_DIR=${PROJ_ROOT}/../dev/faabric/build/static
+    export FAABRIC_BUILD_MOUNT=/build/faabric/static
+
+    if [ ! -d "$FAABRIC_LOCAL_BUILD_DIR" ]; then
+        echo "Unable to find local build dir: $FAABRIC_LOCAL_BUILD_DIR"
+        exit 1
+    fi
 else
     echo "Unrecognised mode: $1"
     exit 1
 fi
 
+pushd ${PROJ_ROOT} >> /dev/null
 
-pushd ${PROJ_ROOT}>>/dev/null
+# Prepare the default version
+VERSION=$(cat VERSION)
+export FAABRIC_CLI_IMAGE=faasm/faabric:${VERSION}
 
-docker-compose -f \
-    dist-test/docker-compose.yml \
+pushd dist-test >> /dev/null
+
+docker-compose \
     run \
     master \
     /code/faabric/dist-test/master.sh
-\
+
+popd >> /dev/null
 
 popd >> /dev/null
