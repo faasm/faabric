6ed4c19721fa5d716aee4bb91ea4587f6f50668c
Simon Shillaker <scs17@ic.ac.uk>
2021-05-19 17:08:04 +0000

    Debug build for dist tests

--- dist-test/build_internal.sh
+++ dist-test/build_internal.sh
@@ -5,8 +5,8 @@ set -e
 export PROJ_ROOT=$(dirname $(dirname $(readlink -f $0)))
 pushd ${PROJ_ROOT}/dist-test >> /dev/null
 
-# Run the build
-inv dev.cmake
+# Run the debug build
+inv dev.cmake --build=Debug
 inv dev.cc faabric_dist_tests
 inv dev.cc faabric_dist_test_server
 
