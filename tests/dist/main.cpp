26aa1f9b7a8cc03c5b1bfa9c851f5018743b67a0
Simon Shillaker <scs17@ic.ac.uk>
2021-06-17 13:26:39 +0000

    Macro-ise

--- tests/dist/main.cpp
+++ tests/dist/main.cpp
@@ -13,36 +13,7 @@
 
 using namespace faabric::scheduler;
 
-struct LogListener : Catch::TestEventListenerBase
-{
-    // Note, we must call base class versions of overridden methods
-    // https://github.com/catchorg/Catch2/pull/1617
-    using TestEventListenerBase::TestEventListenerBase;
-
-    void testCaseStarting(Catch::TestCaseInfo const& testInfo) override
-    {
-        this->Catch::TestEventListenerBase::testCaseStarting(testInfo);
-
-        SPDLOG_INFO("=============================================");
-        SPDLOG_INFO("TEST: {}", testInfo.name);
-        SPDLOG_INFO("=============================================");
-    }
-
-    void sectionStarting(Catch::SectionInfo const& sectionInfo) override
-    {
-        this->Catch::TestEventListenerBase::sectionStarting(sectionInfo);
-
-        // Tests without any sections will be default have one section with the
-        // same name as the test
-        if (sectionInfo.name != currentTestCaseInfo->name) {
-            SPDLOG_INFO("---------------------------------------------");
-            SPDLOG_INFO("SECTION: {}", sectionInfo.name);
-            SPDLOG_INFO("---------------------------------------------");
-        }
-    }
-};
-
-CATCH_REGISTER_LISTENER(LogListener)
+FAABRIC_CATCH_LOGGER
 
 int main(int argc, char* argv[])
 {
