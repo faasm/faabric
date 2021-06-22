4b6fd9e48ba4f61801e04e47f09b876df82e3a31
Simon Shillaker <scs17@ic.ac.uk>
2021-06-22 16:50:16 +0000

    Restore simple class wrapper around context for shutdown

--- src/transport/MessageContext.cpp
+++ src/transport/MessageContext.cpp
@@ -4,7 +4,23 @@
 
 namespace faabric::transport {
 
-static std::shared_ptr<zmq::context_t> instance = nullptr;
+class ContextWrapper
+{
+  public:
+    std::shared_ptr<zmq::context_t> ctx;
+    ContextWrapper()
+    {
+        ctx = std::make_shared<zmq::context_t>(ZMQ_CONTEXT_IO_THREADS);
+    }
+
+    ~ContextWrapper()
+    {
+        SPDLOG_TRACE("Destroying ZeroMQ context");
+        ctx->shutdown();
+    }
+};
+
+static std::shared_ptr<ContextWrapper> instance = nullptr;
 static std::shared_mutex mx;
 
 std::shared_ptr<zmq::context_t> getGlobalMessageContext()
@@ -12,13 +28,13 @@ std::shared_ptr<zmq::context_t> getGlobalMessageContext()
     if (instance == nullptr) {
         faabric::util::FullLock lock(mx);
         if (instance == nullptr) {
-            instance = std::make_shared<zmq::context_t>(ZMQ_CONTEXT_IO_THREADS);
+            instance = std::make_shared<ContextWrapper>();
         }
     }
 
     {
         faabric::util::SharedLock lock(mx);
-        return instance;
+        return instance->ctx;
     }
 }
 }
