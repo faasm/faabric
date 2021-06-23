317bf5ea1e3bae9b8da62ce3dac441cbc5129b2a
Simon Shillaker <scs17@ic.ac.uk>
2021-06-23 09:27:56 +0000

    Simplify global context

--- src/transport/context.cpp
+++ src/transport/context.cpp
@@ -10,62 +10,39 @@ namespace faabric::transport {
  * use one IO thread per Gbps of data.
  */
 
-class ContextWrapper
-{
-  public:
-    std::shared_ptr<zmq::context_t> ctx;
-
-    ContextWrapper()
-    {
-        ctx = std::make_shared<zmq::context_t>(ZMQ_CONTEXT_IO_THREADS);
-    }
-
-    ~ContextWrapper()
-    {
-        SPDLOG_TRACE("Destroying ZeroMQ context");
-
-        // Force outstanding ops to return ETERM
-        ctx->shutdown();
-
-        // Close the context
-        ctx->close();
-    }
-};
-
-static std::shared_ptr<ContextWrapper> instance = nullptr;
+static std::shared_ptr<zmq::context_t> instance = nullptr;
 
 void initGlobalMessageContext()
 {
     if (instance != nullptr) {
-        throw std::runtime_error("Must not initialise global context twice");
+        throw std::runtime_error("Trying to initialise ZeroMQ context twice");
     }
 
     SPDLOG_TRACE("Initialising global ZeroMQ context");
-    instance = std::make_shared<ContextWrapper>();
+    instance = std::make_shared<zmq::context_t>(ZMQ_CONTEXT_IO_THREADS);
 }
 
 std::shared_ptr<zmq::context_t> getGlobalMessageContext()
 {
     if (instance == nullptr) {
-        throw std::runtime_error(
-          "Must explicitly initialise and close global message context");
+        throw std::runtime_error("Trying to access uninitialised ZeroMQ context");
     }
 
-    return instance->ctx;
+    return instance;
 }
 
 void closeGlobalMessageContext()
 {
     if (instance == nullptr) {
-        throw std::runtime_error("Cannot close an uninitialised context");
+        throw std::runtime_error("Cannot close uninitialised ZeroMQ context");
     }
 
     SPDLOG_TRACE("Destroying global ZeroMQ context");
 
     // Force outstanding ops to return ETERM
-    instance->ctx->shutdown();
+    instance->shutdown();
 
     // Close the context
-    instance->ctx->close();
+    instance->close();
 }
 }
