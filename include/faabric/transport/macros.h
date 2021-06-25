49b14107ef5afe2b6919782735aeab9c9ed59de5
Simon Shillaker <scs17@ic.ac.uk>
2021-06-25 07:06:45 +0000

    Remove unused macros

--- include/faabric/transport/macros.h
+++ include/faabric/transport/macros.h
@@ -1,36 +1,7 @@
 #pragma once
 
-#define SEND_MESSAGE(header, msg)                                              \
-    sendHeader(header);                                                        \
-    size_t msgSize = msg.ByteSizeLong();                                       \
-    {                                                                          \
-        uint8_t sMsg[msgSize];                                                 \
-        if (!msg.SerializeToArray(sMsg, msgSize)) {                            \
-            throw std::runtime_error("Error serialising message");             \
-        }                                                                      \
-        send(sMsg, msgSize);                                                   \
-    }
-
-#define SEND_MESSAGE_PTR(header, msg)                                          \
-    sendHeader(header);                                                        \
-    size_t msgSize = msg->ByteSizeLong();                                      \
-    {                                                                          \
-        uint8_t sMsg[msgSize];                                                 \
-        if (!msg->SerializeToArray(sMsg, msgSize)) {                           \
-            throw std::runtime_error("Error serialising message");             \
-        }                                                                      \
-        send(sMsg, msgSize);                                                   \
-    }
-
-
 #define PARSE_MSG(T, data, size)                                               \
     T msg;                                                                     \
     if (!msg.ParseFromArray(data, size)) {                                     \
-        throw std::runtime_error("Error deserialising message");               \
-    }
-
-#define PARSE_RESPONSE(T, data, size)                                          \
-    T response;                                                                \
-    if (!response.ParseFromArray(data, size)) {                                \
         throw std::runtime_error("Error deserialising message");               \
     }
