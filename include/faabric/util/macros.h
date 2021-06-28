b656c49efa3b9c6d590059c24e057bdd9b372a83
Simon Shillaker <scs17@ic.ac.uk>
2021-06-28 10:56:54 +0000

    Formatting

--- include/faabric/util/macros.h
+++ include/faabric/util/macros.h
@@ -5,6 +5,6 @@
 #define BYTES(arr) reinterpret_cast<uint8_t*>(arr)
 #define BYTES_CONST(arr) reinterpret_cast<const uint8_t*>(arr)
 
-#define SLEEP_MS(ms) usleep((ms) * 1000)
+#define SLEEP_MS(ms) usleep((ms)*1000)
 
 #define UNUSED(x) (void)(x)
