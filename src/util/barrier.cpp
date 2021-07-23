9797b134116d6213576ab99514d7b8570762650e
Simon Shillaker <554768+Shillaker@users.noreply.github.com>
2021-07-23 11:10:25 +0200

    Add barrier (#131)
    * Reinstate barrier
    
    * typo in constant name

---
+++ src/util/barrier.cpp
@@ -0,0 +1,51 @@
+#include <faabric/util/barrier.h>
+#include <faabric/util/locks.h>
+#include <faabric/util/logging.h>
+
+namespace faabric::util {
+
+std::shared_ptr<Barrier> Barrier::create(
+  int count,
+  std::function<void()> completionFunctionIn,
+  int timeoutMs)
+{
+    return std::make_shared<Barrier>(count, completionFunctionIn, timeoutMs);
+}
+
+std::shared_ptr<Barrier> Barrier::create(int count, int timeoutMs)
+{
+    return std::make_shared<Barrier>(
+      count, []() {}, timeoutMs);
+}
+
+Barrier::Barrier(int countIn,
+                 std::function<void()> completionFunctionIn,
+                 int timeoutMsIn)
+  : count(countIn)
+  , completionFunction(completionFunctionIn)
+  , timeoutMs(timeoutMsIn)
+{}
+
+void Barrier::wait()
+{
+    UniqueLock lock(mx);
+
+    visits++;
+    int phaseCompletionVisits = currentPhase * count;
+
+    if (visits == phaseCompletionVisits) {
+        completionFunction();
+        currentPhase++;
+        cv.notify_all();
+    } else {
+        auto timePoint = std::chrono::system_clock::now() +
+                         std::chrono::milliseconds(timeoutMs);
+
+        if (!cv.wait_until(lock, timePoint, [this, phaseCompletionVisits] {
+                return visits >= phaseCompletionVisits;
+            })) {
+            throw std::runtime_error("Barrier timed out");
+        }
+    }
+}
+}
