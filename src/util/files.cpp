0a35ed15082d7d41bfea7b72840481dd39c51eb1
Jakub Szewczyk <git@kubasz.xyz>
2021-08-26 20:40:44 +0100

    Significantly faster (10x) file to bytes reading

--- src/util/files.cpp
+++ src/util/files.cpp
@@ -8,6 +8,10 @@
 #include <sstream>
 #include <string>
 
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
 namespace faabric::util {
 std::string readFileToString(const std::string& path)
 {
@@ -21,25 +25,30 @@ std::string readFileToString(const std::string& path)
 
 std::vector<uint8_t> readFileToBytes(const std::string& path)
 {
-    std::ifstream file(path, std::ios::binary);
-
-    // Stop eating new lines in binary mode
-    file.unsetf(std::ios::skipws);
-
-    // Reserve space
-    std::streampos fileSize;
-    file.seekg(0, std::ios::end);
-    fileSize = file.tellg();
-
+    int fd = open(path.c_str(), O_RDONLY);
+    if (fd < 0) {
+        throw std::runtime_error("Couldn't open file " + path);
+    }
+    struct stat statbuf;
+    int staterr = fstat(fd, &statbuf);
+    if (staterr < 0) {
+        throw std::runtime_error("Couldn't stat file " + path);
+    }
+    size_t fsize = statbuf.st_size;
+    posix_fadvise(fd, 0, 0, POSIX_FADV_SEQUENTIAL);
     std::vector<uint8_t> result;
-    result.reserve(fileSize);
-
-    // Read the data
-    file.seekg(0, std::ios::beg);
-    result.insert(result.begin(),
-                  std::istreambuf_iterator<char>(file),
-                  std::istreambuf_iterator<char>());
-
+    result.resize(fsize);
+    int cpos = 0;
+    while (cpos < fsize) {
+        int rc = read(fd, result.data(), fsize - cpos);
+        if (rc < 0) {
+            perror("Couldn't read file");
+            throw std::runtime_error("Couldn't read file " + path);
+        } else {
+            cpos += rc;
+        }
+    }
+    close(fd);
     return result;
 }
 
