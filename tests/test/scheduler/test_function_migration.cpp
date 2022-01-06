69216b87a374388b79c8e173b51a88b3e39232f2
Carlos Segarra <carlos@carlossegarra.com>
2022-01-06 16:12:56 +0000

    tests: add more complex migration opportunity detection test

--- tests/test/scheduler/test_function_migration.cpp
+++ tests/test/scheduler/test_function_migration.cpp
@@ -29,7 +29,7 @@ class FunctionMigrationTestFixture
 
 TEST_CASE_METHOD(FunctionMigrationTestFixture,
                  "Test starting and stopping the function migration server",
-                 "[scheduler][migration]")
+                 "[scheduler]")
 {
     SECTION("Disable function migration")
     {
@@ -49,7 +49,7 @@ TEST_CASE_METHOD(FunctionMigrationTestFixture,
 TEST_CASE_METHOD(
   FunctionMigrationTestFixture,
   "Test can't run function migration server with non-positive check period",
-  "[scheduler][migration]")
+  "[scheduler]")
 {
     conf.migrationCheckPeriod = 0;
 
@@ -59,7 +59,7 @@ TEST_CASE_METHOD(
 TEST_CASE_METHOD(
   FunctionMigrationTestFixture,
   "Test function migration server detects migration opportunities",
-  "[scheduler][migration]")
+  "[scheduler]")
 {
     // First set resources before calling the functions: one will be allocated
     // locally, another one in the remote host
@@ -73,18 +73,7 @@ TEST_CASE_METHOD(
 
     // Set up expectations
     std::shared_ptr<faabric::PendingMigrations> expectedMigrations;
-    SECTION("Can not migrate")
-    {
-        // As functions in the test executor run for a short time, we amend
-        // the local resources to make it look as though the request is still
-        // being executed
-        faabric::HostResources r;
-        r.set_slots(1);
-        r.set_usedslots(1);
-        sch.setThisHostResources(r);
-
-        expectedMigrations = nullptr;
-    }
+    SECTION("Can not migrate") { expectedMigrations = nullptr; }
 
     SECTION("Can migrate")
     {
@@ -101,6 +90,82 @@ TEST_CASE_METHOD(
         migration->set_messageid(req->messages().at(1).id());
         migration->set_srchost(hosts.at(1));
         migration->set_dsthost(hosts.at(0));
+        expectedMigrations =
+          std::make_shared<faabric::PendingMigrations>(expected);
+    }
+
+    sch.checkForMigrationOpportunities();
+
+    auto actualMigrations = sch.canAppBeMigrated(appId);
+    if (expectedMigrations == nullptr) {
+        REQUIRE(actualMigrations == expectedMigrations);
+    } else {
+        REQUIRE(actualMigrations->appid() == expectedMigrations->appid());
+        REQUIRE(actualMigrations->migrations_size() ==
+                expectedMigrations->migrations_size());
+        for (int i = 0; i < actualMigrations->migrations_size(); i++) {
+            auto actual = actualMigrations->mutable_migrations()->at(i);
+            auto expected = expectedMigrations->mutable_migrations()->at(i);
+            REQUIRE(actual.messageid() == expected.messageid());
+            REQUIRE(actual.srchost() == expected.srchost());
+            REQUIRE(actual.dsthost() == expected.dsthost());
+        }
+    }
+
+    faabric::Message res = sch.getFunctionResult(req->messages().at(0).id(),
+                                                 2 * SHORT_TEST_TIMEOUT_MS);
+    REQUIRE(res.returnvalue() == 0);
+
+    // Check that after the result is set, the app can't be migrated no more
+    sch.checkForMigrationOpportunities();
+    REQUIRE(sch.canAppBeMigrated(appId) == nullptr);
+}
+
+TEST_CASE_METHOD(
+  FunctionMigrationTestFixture,
+  "Test detecting migration opportunities with several hosts and requests",
+  "[scheduler]")
+{
+    // First set resources before calling the functions: one will be allocated
+    // locally, another one in the remote host
+    std::vector<std::string> hosts = { masterHost, "hostA", "hostB", "hostC" };
+    std::vector<int> slots = { 1, 1, 1, 1 };
+    setHostResources(hosts, slots);
+
+    auto req = faabric::util::batchExecFactory("foo", "migration", 4);
+    auto decision = sch.callFunctions(req);
+    uint32_t appId = req->messages().at(0).appid();
+
+    // Set up expectations
+    std::shared_ptr<faabric::PendingMigrations> expectedMigrations;
+    SECTION("Can not migrate") { expectedMigrations = nullptr; }
+
+    SECTION("Can migrate")
+    {
+        // Update host resources so that a migration opportunity appears
+        faabric::HostResources r;
+        r.set_slots(2);
+        r.set_usedslots(1);
+        // This host
+        sch.setThisHostResources(r);
+        // Host A
+        faabric::scheduler::queueResourceResponse("hostA", r);
+
+        // Build expected result
+        faabric::PendingMigrations expected;
+        expected.set_appid(appId);
+        // Migrate last message (scheduled to last host) to first host. This
+        // fills up the first host.
+        auto* migration1 = expected.add_migrations();
+        migration1->set_messageid(req->messages().at(3).id());
+        migration1->set_srchost(hosts.at(3));
+        migration1->set_dsthost(hosts.at(0));
+        // Migrate penultimate message (scheduled to penultimate host) to first
+        // host. This fills up the first host.
+        auto* migration2 = expected.add_migrations();
+        migration2->set_messageid(req->messages().at(2).id());
+        migration2->set_srchost(hosts.at(2));
+        migration2->set_dsthost(hosts.at(1));
         expectedMigrations =
           std::make_shared<faabric::PendingMigrations>(expected);
     }
