b05267c50159feda5b4bbcc7b21c2adf38f30a30
Simon Shillaker <mail@simonshillaker.com>
2022-02-11 10:41:43 +0100

    Populate classes with members, rtd theme

--- docs/conf.py
+++ docs/conf.py
@@ -2,6 +2,11 @@ from os.path import dirname, realpath, join
 from subprocess import run
 from os import makedirs
 
+# ----------------------------------------
+# This is the Sphinx configuration file, which wraps
+# the calls to Doxygen and Breathe.
+# ----------------------------------------
+
 DOCS_ROOT = dirname(realpath(__file__))
 DOXYGEN_OUT = join(DOCS_ROOT, "doxygen", "xml")
 APIDOC_OUT_DIR = join(DOCS_ROOT, "apidoc")
@@ -11,7 +16,9 @@ makedirs(APIDOC_OUT_DIR, exist_ok=True)
 run("doxygen", cwd=DOCS_ROOT, check=True, shell=True)
 
 run(
-    "breathe-apidoc {} -o {}".format(DOXYGEN_OUT, APIDOC_OUT_DIR),
+    "breathe-apidoc {} -o {} -f -m".format(
+        DOXYGEN_OUT, APIDOC_OUT_DIR
+    ),
     cwd=DOCS_ROOT,
     check=True,
     shell=True,
@@ -21,14 +28,16 @@ project = "Faabric"
 copyright = "2022, Simon Shillaker"
 author = "Simon Shillaker"
 
-extensions = ["breathe", "myst_parser"]
+extensions = ["breathe", "myst_parser", "sphinx_rtd_theme"]
 
 templates_path = ["source/templates"]
 html_static_path = ["source/static"]
 
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 
 breathe_projects = {"Faabric": DOXYGEN_OUT}
 breathe_default_project = "Faabric"
+breathe_default_members = ("members", "undoc-members")
+
