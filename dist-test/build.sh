f5240802bf0a3db7a2b593c2fd34e489029750d2
Simon Shillaker <554768+Shillaker@users.noreply.github.com>
2022-02-16 13:32:15 +0100

    Conan cache improvements (#230)
    * Tweaks to conan cache setup
    
    * Set conan cache mount env var in GHA

--- dist-test/build.sh
+++ dist-test/build.sh
@@ -5,10 +5,6 @@ set -e
 export PROJ_ROOT=$(dirname $(dirname $(readlink -f $0)))
 pushd ${PROJ_ROOT} >> /dev/null
 
-export CONAN_CACHE_MOUNT_SOURCE=$HOME/.conan/
-# Ensure the cache directory exists before starting the containers
-mkdir -p ${CONAN_CACHE_MOUNT_SOURCE}
-
 # Run the build
 docker-compose \
     run \
