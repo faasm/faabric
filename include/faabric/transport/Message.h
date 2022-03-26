48612c9729667c76c601c2ab758337487171b90f
Simon Shillaker <mail@simonshillaker.com>
2022-03-26 12:43:43 +0100

    Docs

--- include/faabric/transport/Message.h
+++ include/faabric/transport/Message.h
@@ -4,15 +4,24 @@
 #include <zmq.hpp>
 
 namespace faabric::transport {
-/* Wrapper arround zmq::message_t
+/**
+ * Represents message data passed around the transport layer. Essentially an
+ * array of bytes, with a size and a flag to say whether there's more data to
+ * follow.
  *
- * Thin abstraction around 0MQ's message type. Represents an array of bytes,
- * its size, and other traits from the underlying type useful to faabric.
+ * Currently it's just a thin wrapper around a ZeroMQ message that avoids us
+ * being dependent on the ZeroMQ API outside of boilerplate code.
  */
 class Message
 {
   public:
-    explicit Message(zmq::message_t &&msgIn);
+    /**
+     * Creates a message from a ZeroMQ message. Importantly avoids a copy by
+     * using the move constructor of zmq::message_t
+     *
+     * https://github.com/zeromq/cppzmq/blob/master/zmq.hpp#L408
+     */
+    explicit Message(zmq::message_t&& msgIn);
 
     // Empty message signals shutdown
     Message() = default;
