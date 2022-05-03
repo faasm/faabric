4bc3343ba7b58565b0aa9387fce04f5304c4ffbb
Carlos Segarra <carlos@carlossegarra.com>
2022-05-03 15:57:36 +0000

    dist-tests: add distributed MPI test for two worlds spanning two hosts but with different master hosts

--- tests/dist/mpi/test_multiple_mpi_worlds.cpp
+++ tests/dist/mpi/test_multiple_mpi_worlds.cpp
@@ -34,4 +34,49 @@ TEST_CASE_METHOD(MpiDistTestsFixture,
     checkAllocationAndResult(req1, 15000);
     checkAllocationAndResult(req2, 15000);
 }
+
+TEST_CASE_METHOD(MpiDistTestsFixture,
+                 "Test concurrent MPI applications with different master host",
+                 "[mpi]")
+{
+    // Prepare the first request (local): 2 ranks locally, 2 remotely
+    int worldSize = 4;
+    setLocalSlots(2, worldSize);
+    auto req1 = setRequest("alltoall-sleep");
+    sch.callFunctions(req1);
+
+    // Sleep for a bit to allow for the scheduler to schedule all MPI ranks
+    SLEEP_MS(200);
+
+    // Prepare the second request (remote): 4 ranks remotely, 2 locally
+    int newWorldSize = 6;
+    setLocalSlots(2, newWorldSize);
+    auto req2 = setRequest("alltoall-sleep");
+    // Request remote execution
+    faabric::scheduler::FunctionCallClient cli(getWorkerIP());
+    cli.executeFunctions(req2);
+
+    // Skip the automated exec graph check, and check manually
+    bool skipExecGraphCheck = true;
+    checkAllocationAndResult(req1, 15000, skipExecGraphCheck);
+    checkAllocationAndResult(req2, 15000, skipExecGraphCheck);
+
+    // Check exec graph for first request
+    auto execGraph1 =
+      sch.getFunctionExecGraph(req1->mutable_messages()->at(0).id());
+    std::vector<std::string> expectedHosts1 = {
+        getMasterIP(), getMasterIP(), getWorkerIP(), getWorkerIP()
+    };
+    REQUIRE(expectedHosts1 ==
+            faabric::scheduler::getMpiRankHostsFromExecGraph(execGraph1));
+
+    // Check exec graph for second request
+    auto execGraph2 =
+      sch.getFunctionExecGraph(req2->mutable_messages()->at(0).id());
+    std::vector<std::string> expectedHosts2 = { getWorkerIP(), getWorkerIP(),
+                                                getMasterIP(), getMasterIP(),
+                                                getWorkerIP(), getWorkerIP() };
+    REQUIRE(expectedHosts2 ==
+            faabric::scheduler::getMpiRankHostsFromExecGraph(execGraph2));
+}
 }
