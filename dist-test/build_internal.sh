a199b7384dcda7b8c5cae320898a7c8629dfd60a
Carlos Segarra <carlos@carlossegarra.com>
2022-10-03 16:23:29 +0000

    fix(dist-tests): use venv in dist-tests' build internal

--- dist-test/build_internal.sh
+++ dist-test/build_internal.sh
@@ -5,6 +5,9 @@ set -e
 export PROJ_ROOT=$(dirname $(dirname $(readlink -f $0)))
 pushd ${PROJ_ROOT} >> /dev/null
 
+# Activate the Python venv
+source ./bin/workon.sh
+
 # Run the debug build
 inv dev.cmake --build=Debug
 inv dev.cc faabric_dist_tests
