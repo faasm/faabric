8d414967237642173eeb6f7b06b3572dca6ed56d
Carlos Segarra <carlos@carlossegarra.com>
2023-06-19 11:18:08 +0000

    fixtures: remove scheduler fixture from exec-context test fixture

--- tests/utils/fixtures.h
+++ tests/utils/fixtures.h
@@ -281,7 +281,7 @@ class PointToPointClientServerFixture
     faabric::transport::PointToPointServer server;
 };
 
-class ExecutorContextTestFixture : public SchedulerTestFixture
+class ExecutorContextTestFixture
 {
   public:
     ExecutorContextTestFixture() {}
