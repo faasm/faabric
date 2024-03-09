134287d52b1afb2b12c2c536181ed13e03765da5
lg <lg4869@outlook.com>
2024-03-09 00:30:33 +0000

    fix format

--- tests/dist/mpi/native/main.cpp
+++ tests/dist/mpi/native/main.cpp
@@ -7,7 +7,3 @@ int main(int argc, char* argv[])
     tests::mpi::bench_allreduce();
     return 0;
 }
-
-/*
-docker run --user mpirun --rm -it --entrypoint bash faasm.azurecr.io/openmpi-worker:0.13.1
-*/
