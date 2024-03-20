86a8e7647807c062bd7170440aad6746151f92a1
Carlos Segarra <carlos@carlossegarra.com>
2024-03-20 16:23:49 +0000

    mpi: make struct 8-byte aligned

--- include/faabric/mpi/MpiMessage.h
+++ include/faabric/mpi/MpiMessage.h
@@ -21,6 +21,18 @@ enum MpiMessageType : int32_t
     BROADCAST = 11,
 };
 
+/* Simple fixed-size C-struct to capture the state of an MPI message moving
+ * through Faabric.
+ *
+ * We require fixed-size, and no unique pointers to be able to use
+ * high-throughput in-memory ring-buffers to send the messages around.
+ * This also means that we manually malloc/free the data pointer. The message
+ * size is:
+ * 7 * int32_t = 7 * 4 bytes = 28 bytes
+ * 1 * int32_t (padding) = 4 bytes
+ * 1 * void* = 1 * 8 bytes = 8 bytes
+ * total = 40 bytes = 5 * 8 so the struct is 8 byte-aligned
+ */
 struct MpiMessage
 {
     int32_t id;
@@ -30,8 +42,10 @@ struct MpiMessage
     int32_t typeSize;
     int32_t count;
     MpiMessageType messageType;
+    int32_t __make_8_byte_aligned;
     void* buffer;
 };
+static_assert((sizeof(MpiMessage) % 8) == 0, "MPI message must be 8-aligned!");
 
 inline size_t payloadSize(const MpiMessage& msg)
 {
